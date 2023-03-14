e90dada5fa61abf963b8fc5663272f28f041d00f
Adnaan-Shafique <125882882+Adnaan-Shafique@users.noreply.github.com>
2023-03-14 17:55:54 +0530

    Update Automatic File Sorter in File Explorer.py

--- Automatic File Sorter in File Explorer.py
+++ Automatic File Sorter in File Explorer.py
@@ -1,38 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
+import os, shutil    #imports os and shell utilities(used to make changes in a directory)
 
-# In[1]:
+path = r"C:/Users/Welcome/Music/sample/"   #define which path you want this to run in, r in the front is to take it as raw string and neglect \, /
 
+file_name = os.listdir(path)   #pulls out the file names from the path
 
+folder_names = ['csv files','image files','text files']   #folder names that we will need
 
-
-
-# In[9]:
-
-
-
-
-
-# In[13]:
-
-
-
-
-
-# In[20]:
-
-
-import os, shutil
-
-path = r"C:/Users/Welcome/Music/sample/"
-
-file_name = os.listdir(path)
-
-folder_names = ['csv files','image files','text files']
+#for loop for making directories with the folder names if they are not created initially
 
 for loop in range(0,3):
     if not os.path.exists(path + folder_names[loop]):
         os.makedirs(path + folder_names[loop])
+        
+#for loop for moving the respective files into the respective folders
         
 for file in file_name:
     if ".csv" in file and not os.path.exists(path + "csv files/" + file):
@@ -41,40 +21,5 @@ for file in file_name:
         shutil.move(path + file,path + "image files/" + file)
     elif ".txt" in file and not os.path.exists(path + "text files/" + file):
         shutil.move(path + file,path + "text files/" + file)
-
-
-# In[18]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
